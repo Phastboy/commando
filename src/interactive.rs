2792e964f88f72bbf5bc5084cfbd9e4472816d96
Hammed Anuoluwapo Pelumi <stationphast@gmail.com>
2025-01-08 00:24:17 +0100

    commit type

--- src/interactive.rs
+++ src/interactive.rs
@@ -1,3 +1,16 @@
+use dialoguer::Select;
+
 pub fn run_interactive() {
-    println!("Interactive commit flow coming soon!");
+    // commit types
+    let commit_types = vec!["feat", "fix", "chore", "docs", "refactor", "test"];
+
+    // prompt for commit type
+    let commit_type = Select::new()
+        .with_prompt("Select the type of change")
+        .items(&commit_types)
+        .default(0)
+        .interact()
+        .expect("Failed to select commit type");
+
+    println!("Selected commit type: {}", commit_types[commit_type]);
 }
