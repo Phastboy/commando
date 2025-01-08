28a74205f3ced516c9131cff2acd43769e3e2f33
Hammed Anuoluwapo Pelumi <stationphast@gmail.com>
2025-01-08 01:00:42 +0100

    theme

--- src/interactive.rs
+++ src/interactive.rs
@@ -1,11 +1,14 @@
-use dialoguer::Select;
+use dialoguer::{Select, theme::ColorfulTheme};
 
 pub fn run_interactive() {
+    // theme
+    let theme = ColorfulTheme::default();
+
     // commit types
     let commit_types = vec!["feat", "fix", "chore", "docs", "refactor", "test"];
 
     // prompt for commit type
-    let commit_type = Select::new()
+    let commit_type = Select::with_theme(&theme)
         .with_prompt("Select the type of change")
         .items(&commit_types)
         .default(0)
